20064dc3d9a54e361417effe2f70d917f84337b3
Jennifer Chow <jennego@gmail.com>
2021-08-26 21:32:34 -0700

    begin playing with formik and yup

--- src/pages/membership.js
+++ src/pages/membership.js
@@ -1,7 +1,60 @@
 import React from "react"
 import Layout from "../components/layout"
 import Main from "../components/MainContent"
-import { Parallax, Background } from "react-parallax"
+import { useFormik } from "formik"
+import * as yup from "yup"
+import Button from "@material-ui/core/Button"
+import TextField from "@material-ui/core/TextField"
+
+const validationSchema = yup.object({
+  email: yup
+    .string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required"),
+  name: yup.string("Enter your name").required("Name is required"),
+})
+
+const MemberForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "foobar@example.com",
+      name: "Bob",
+    },
+    validationSchema: validationSchema,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          fullWidth
+          id="name"
+          name="name"
+          label="name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          error={formik.touched.name && Boolean(formik.errors.name)}
+          helperText={formik.touched.name && formik.errors.name}
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  )
+}
 
 const Membership = () => {
   return (
@@ -11,7 +64,7 @@ const Membership = () => {
         <p>Blah blah</p>
         <h2>Membership Form</h2>
 
-        <p>Later</p>
+        <MemberForm />
       </Main>
     </Layout>
   )
