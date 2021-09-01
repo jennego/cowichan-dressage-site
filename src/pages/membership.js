bff081087721a4c518268b3dc872ad42c5326de2
Jennifer Chow <jennego@gmail.com>
2021-08-31 23:11:54 -0700

    got form with datepicker to work

--- src/pages/membership.js
+++ src/pages/membership.js
@@ -19,11 +19,16 @@ const BirthDatePickerField = ({ field, form, ...other }) => {
 
   return (
     <KeyboardDatePicker
+      fullWidth
       clearable
-      disablePast
+      label="Birth Date"
+      placeholder="Click icon to select date"
+      disableFuture
       name={field.name}
       value={field.value}
-      format="dd/MM/yyyy"
+      views={["year", "month", "date"]}
+      openTo="year"
+      format="MM/dd/yyyy"
       helperText={currentError}
       error={Boolean(currentError)}
       onError={error => {
@@ -45,102 +50,157 @@ const validationSchema = yup.object({
     .email("Enter a valid email")
     .required("Email is required"),
   name: yup.string("Enter your name").required("Name is required"),
+  address: yup.string("Enter your address"),
+  birthDate: yup.date("Enter a date"),
+  hcbc: yup.number().typeError("Needs to be a number"),
 })
 
 const MemberForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: "foobar@example.com",
-      name: "Bob",
-      birthDate: new Date(),
-    },
-    validationSchema: validationSchema,
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2))
-    },
-  })
+  // const formik = useFormik({
+  //   initialValues: {
+  //     email: "foobar@example.com",
+  //     name: "Bob",
+  //     birthDate: new Date(),
+  //   },
+  //   validationSchema: validationSchema,
+  //   onSubmit: values => {
+  //     alert(JSON.stringify(values, null, 2))
+  //   },
+  // })
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
-        <Grid container spacing={3}>
-          <Grid item md={6}>
-            <TextField
-              fullWidth
-              id="name"
-              name="name"
-              label="Name"
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              error={formik.touched.name && Boolean(formik.errors.name)}
-              helperText={formik.touched.name && formik.errors.name}
-            />
-          </Grid>
-          <Grid item md={6}>
-            <TextField
-              fullWidth
-              id="email"
-              name="email"
-              label="Email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-            />
-          </Grid>
-          <Grid item md={6}>
-            <TextField
-              fullWidth
-              id="address"
-              name="address"
-              label="Address"
-              value={formik.values.address}
-              onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.address)}
-              helperText={formik.touched.email && formik.errors.address}
-            />
-          </Grid>
-          <Grid item md={6}>
-            <TextField
-              fullWidth
-              id="phone-number"
-              name="phone-number"
-              label="Phone Number"
-              value={formik.values.phonenumber}
-              onChange={formik.handleChange}
-              error={
-                formik.touched.phonenumber && Boolean(formik.errors.phonenumber)
-              }
-              helperText={
-                formik.touched.phonenumber && formik.errors.phonenumber
-              }
-            />
-          </Grid>
-          <Grid item md={6}>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-              {/* <Field name="birthDate" component={DatePickerField} /> */}
-            </MuiPickersUtilsProvider>
-            {/* <TextField
+    <Formik
+      onSubmit={values => {
+        alert(JSON.stringify(values, null, 2))
+      }}
+      validationSchema={validationSchema}
+      initialValues={{
+        email: "",
+        name: "",
+        address: "",
+        phonenumber: "",
+        birthDate: null,
+      }}
+    >
+      {props => (
+        <Form>
+          {console.log("formik props", props)}
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <TextField
                 fullWidth
-                id="birthdate"
-                name="birthdate"
-                label="Birth Date"
-                value={formik.values.phonenumber}
-                onChange={formik.handleChange}
+                id="name"
+                name="name"
+                label="Name"
+                value={props.values.name}
+                onChange={props.handleChange}
+                error={props.touched.name && Boolean(props.errors.name)}
+                helperText={props.touched.name && props.errors.name}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                id="email"
+                name="email"
+                label="Email"
+                value={props.values.email}
+                onChange={props.handleChange}
+                error={props.touched.email && Boolean(props.errors.email)}
+                helperText={props.touched.email && props.errors.email}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                id="address"
+                name="address"
+                label="Address"
+                value={props.values.address}
+                onChange={props.handleChange}
+                error={props.touched.email && Boolean(props.errors.address)}
+                helperText={props.touched.email && props.errors.address}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                id="phone-number"
+                name="phone-number"
+                label="Phone Number"
+                value={props.values.phonenumber}
+                onChange={props.handleChange}
                 error={
-                  formik.touched.phonenumber &&
-                  Boolean(formik.errors.phonenumber)
+                  props.touched.phonenumber && Boolean(props.errors.phonenumber)
                 }
                 helperText={
-                  formik.touched.phonenumber && formik.errors.phonenumber
+                  props.touched.phonenumber && props.errors.phonenumber
                 }
-              /> */}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Field name="birthDate" component={BirthDatePickerField} />
+              </MuiPickersUtilsProvider>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                id="hcbc"
+                name="hcbc"
+                label="Horse Council BC Member Number"
+                value={props.values.hcbc}
+                onChange={props.handleChange}
+                error={props.touched.hcbc && Boolean(props.errors.hcbc)}
+                helperText={props.touched.hcbc && props.errors.hcbc}
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                id="emergContactName"
+                name="emergContactName"
+                label="Emergancy Contact Name"
+                value={props.values.emergContactName}
+                onChange={props.handleChange}
+                error={
+                  props.touched.emergContactName &&
+                  Boolean(props.errors.emergContactName)
+                }
+                helperText={
+                  props.touched.emergContactName &&
+                  props.errors.emergContactName
+                }
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                id="emergContactPh"
+                name="emergContactPh"
+                label="Emergency Contact Phone Number"
+                value={props.values.emergContactPh}
+                onChange={props.handleChange}
+                error={
+                  props.touched.emergContactPh &&
+                  Boolean(props.errors.emergContactPh)
+                }
+                helperText={
+                  props.touched.emergContactPh && props.errors.emergContactPh
+                }
+              />
+            </Grid>
           </Grid>
-        </Grid>
-        <Button color="primary" variant="contained" type="submit">
-          Submit
-        </Button>
-      </form>
-    </div>
+          <div style={{ marginTop: "2rem" }}>
+            <Button color="primary" variant="contained" type="submit">
+              Submit
+            </Button>
+          </div>
+        </Form>
+      )}
+    </Formik>
   )
 }
 
