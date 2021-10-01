1132289fb25b9f56acc41473b4a28b500f62ae43
Jennifer Chow <jennego@gmail.com>
2021-09-30 20:13:38 -0700

    style 404 page a bit, finally file upload works

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,21 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Main from "../components/MainContent"
+import { Typography, Button } from "@material-ui/core"
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Seo title="404: Not found" />'
+    <Main>
+      <Typography variant="h1">404: Not Found</Typography>
+      <Typography gutterBottom>
+        You just hit a url that doesn&#39;t exist... the sadness. 🙁
+      </Typography>
+      <Button variant="contained" color="primary" href="/">
+        Go to home page
+      </Button>
+    </Main>
   </Layout>
 )
 
