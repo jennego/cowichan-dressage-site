d258b50a451fa01dc7b80cb8f920d4c931778975
Jennifer Chow <jennego@gmail.com>
2021-12-04 22:42:42 -0800

    also get rid of warning from event template

--- src/components/eventAccordion.js
+++ src/components/eventAccordion.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect } from "react"
+import React, { useState, useLayoutEffect } from "react"
 
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
