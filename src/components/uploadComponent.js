420683d34c25c538eb760a79b73fd46b2a142f37
Jennifer Chow <jennego@gmail.com>
2022-02-21 01:24:02 -0800

    do not commit this

--- src/components/uploadComponent.js
+++ src/components/uploadComponent.js
@@ -8,7 +8,7 @@ import Button from "@material-ui/core/Button"
 import PDFListItem from "./pdfListItem"
 import { Typography, Paper, Grid, FormHelperText } from "@material-ui/core"
 
-const UploadField = ({ index, doc, props, waiverType }) => {
+const UploadField = ({ index, props, waiverType }) => {
   const {
     getRootProps,
     getInputProps,
@@ -16,7 +16,7 @@ const UploadField = ({ index, doc, props, waiverType }) => {
     acceptedFiles,
     fileRejections,
   } = useDropzone({
-    accept: ".pdf",
+    // accept: ".pdf",
     multiple: false,
     onDrop: acceptedFiles => {
       props.setFieldValue(`waiver${index + 1}`, acceptedFiles[0])
@@ -28,87 +28,84 @@ const UploadField = ({ index, doc, props, waiverType }) => {
   return (
     <div>
       <Paper elevation={5} className="pdf-item">
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
+        {/* {/* <Grid container spacing={1}> */}
+        {/* <Grid item xs={12}>
             <FormLabel>
               Download
               <span style={{ fontStyle: "italic" }}> {doc.title} </span>
-            </FormLabel>
-            <PDFListItem file={doc.file} />
-          </Grid>
-          <Grid item xs={12}>
-            <FormLabel>
-              Upload Completed Form
-              <span style={{ fontStyle: "italic" }}> {doc.title} </span>
-            </FormLabel>
-            <div
-              className={
-                Boolean(props.errors[`waiver${index + 1}`]) &&
-                props.touched[`waiver${index + 1}`]
-                  ? "error-border"
-                  : "dashed-border"
-              }
-            >
-              {acceptedFiles.length === 0 ? (
-                <div {...getRootProps({ className: "dropzone" })}>
-                  <input
-                    {...getInputProps({
-                      name: `waiver${index + 1}`,
-                      id: "`waiver${index + 1}`",
-                    })}
-                  />
-                  {isDragActive ? (
-                    <div style={{ padding: "1rem" }}>
-                      <Typography variant="body1">
-                        Drop the file here ...
-                      </Typography>
-                    </div>
-                  ) : (
-                    <div style={{ padding: "1rem" }}>
-                      {fileRejections.length > 0 ? (
-                        <Alert
-                          severity="error"
-                          style={{ marginBottom: "1rem" }}
-                        >
-                          Please upload a PDF file.
-                        </Alert>
-                      ) : (
-                        ""
-                      )}
-                      <Button variant="contained" color="primary">
-                        Drag and Drop file here or click to upload
-                      </Button>
-                    </div>
-                  )}
-                </div>
-              ) : (
-                <Alert
-                  icon={<PictureAsPdf />}
-                  action={
-                    <Tooltip title="Remove this file">
-                      <Delete
-                        onClick={() => {
-                          acceptedFiles.length = 0
-                          props.setFieldValue(`waiver${index + 1}`, null)
-                        }}
-                      />
-                    </Tooltip>
-                  }
-                >
-                  {acceptedFiles[0].name}
-                  {/* {formik.values[waiver].name} */}
-                </Alert>
-              )}
-            </div>
-            {Boolean(props.errors[`waiver${index + 1}`]) &&
-            props.touched[`waiver${index + 1}`] ? (
-              <FormHelperText error={true} filled={true}>
-                The file {doc.file.fileName} is required.
-              </FormHelperText>
+            </FormLabel> */}
+        {/* <PDFListItem file={doc.file} /> 
+            </Grid> 
+            */}
+        <Grid item xs={12}>
+          <FormLabel>
+            Upload Completed Form
+            <span style={{ fontStyle: "italic" }}> Form here </span>
+          </FormLabel>
+          <div
+            className={
+              Boolean(props.errors[`waiver${index + 1}`]) &&
+              props.touched[`waiver${index + 1}`]
+                ? "error-border"
+                : "dashed-border"
+            }
+          >
+            {acceptedFiles.length === 0 ? (
+              <div {...getRootProps({ className: "dropzone" })}>
+                <input
+                  {...getInputProps({
+                    name: `waiver${index + 1}`,
+                    id: `waiver${index + 1}`,
+                  })}
+                />
+                {isDragActive ? (
+                  <div style={{ padding: "1rem" }}>
+                    <Typography variant="body1">
+                      Drop the file here ...
+                    </Typography>
+                  </div>
+                ) : (
+                  <div style={{ padding: "1rem" }}>
+                    {fileRejections.length > 0 ? (
+                      <Alert severity="error" style={{ marginBottom: "1rem" }}>
+                        Please upload a PDF file.
+                      </Alert>
+                    ) : (
+                      ""
+                    )}
+                    <Button variant="contained" color="primary">
+                      Drag and Drop file here or click to upload
+                    </Button>
+                  </div>
+                )}
+              </div>
             ) : (
-              ""
+              <Alert
+                icon={<PictureAsPdf />}
+                action={
+                  <Tooltip title="Remove this file">
+                    <Delete
+                      onClick={() => {
+                        acceptedFiles.length = 0
+                        props.setFieldValue(`waiver${index + 1}`, null)
+                      }}
+                    />
+                  </Tooltip>
+                }
+              >
+                {acceptedFiles[0].name}
+                {/* {formik.values[waiver].name} */}
+              </Alert>
             )}
-          </Grid>
+          </div>
+          {Boolean(props.errors[`waiver${index + 1}`]) &&
+          props.touched[`waiver${index + 1}`] ? (
+            <FormHelperText error={true} filled={true}>
+              {/* The file {doc.file.fileName} is required. */}
+            </FormHelperText>
+          ) : (
+            ""
+          )}
         </Grid>
       </Paper>
     </div>
@@ -118,7 +115,7 @@ const UploadField = ({ index, doc, props, waiverType }) => {
 export const UploadComponent = ({ props, fileArray, waiverType }) => {
   return (
     <div style={{ marginTop: "1rem" }}>
-      <Typography variant="h5">Waivers</Typography>
+      {/* <Typography variant="h5">Waivers</Typography>
       <div style={{ margin: "0.5rem 0 1rem 0" }}>
         <Typography gutterBottom variant="body2">
           Please download listed PDFs, fill it out (if not a fillable PDF, you
@@ -140,7 +137,9 @@ export const UploadComponent = ({ props, fileArray, waiverType }) => {
       </div>
       {fileArray.map((doc, index) => (
         <UploadField props={props} doc={doc} index={index} />
-      ))}
+      ))} */}
+
+      <UploadField props={props} index={0} />
     </div>
   )
 }
