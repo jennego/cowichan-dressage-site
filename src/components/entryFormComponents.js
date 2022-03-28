d043b99bba2ff977081035bfaaedf29294ce7c53
Jennifer Chow <jennego@gmail.com>
2022-03-27 20:23:44 -0700

    try to change date label still

--- src/components/entryFormComponents.js
+++ src/components/entryFormComponents.js
@@ -21,9 +21,7 @@ export const DateForm = ({ data, props, location }) => {
   return (
     <div style={{ paddingTop: "1rem", paddingBottom: "1rem" }}>
       <FormControl component="fieldset">
-        <FormLabel id="event-date-label" component="legend">
-          Event Date
-        </FormLabel>
+        <FormLabel id="event-date-label">Event Date</FormLabel>
         <RadioGroup
           aria-label="event date"
           aria-labelledby="event-date-label"
